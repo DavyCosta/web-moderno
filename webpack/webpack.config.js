46bc2452bea22bcfcb16bb8a21977bd364614173
Davy <davycabral2014@gmail.com>
2022-05-13 18:49:56 -0300

    finalizando webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,14 +1,36 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            
+            new OptimizeCssAssetsPlugin({})
+        ]
+    },
     plugin: [
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
+        
         new MiniCssExtractPlugin({
             filename: "estilo.css"
 })
